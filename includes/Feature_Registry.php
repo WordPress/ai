bc3aa560b8b348aaee17ca18620fe7b13b8f52cd
Mohamed Khaled <mohamed.khaled@9hdigital.com>
2025-10-12 15:34:15 +0300

    Fix coding standards issues
    - Move wp_ai_feature_registry() from Feature_Registry to Feature_Loader
    - Add missing newline at end of Feature_Loader.php
    - Exclude OneObjectStructurePerFile and SeparateFunctionsFromOO rules
      (intentional design: Feature_Collection in Feature_Registry per reviewer,
       wp_ai_feature_registry() in Feature_Loader for logical grouping)

--- includes/Feature_Registry.php
+++ includes/Feature_Registry.php
@@ -141,20 +141,3 @@ class Feature_Registry {
 		return $this->feature_collection->get_all_features();
 	}
 }
-
-/**
- * Gets the global Feature_Registry instance.
- *
- * @since 0.1.0
- *
- * @return Feature_Registry The Feature_Registry instance.
- */
-function wp_ai_feature_registry(): Feature_Registry {
-	static $instance = null;
-
-	if ( null === $instance ) {
-		$instance = new Feature_Registry();
-	}
-
-	return $instance;
-}
