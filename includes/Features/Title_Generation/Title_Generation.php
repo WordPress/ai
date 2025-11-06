443b29d39bb59273007a7d77e9716b515cd4df63
Darin Kotter <darin.kotter@gmail.com>
2025-11-06 15:18:04 -0700

    Load the assets in our feature

--- includes/Features/Title_Generation/Title_Generation.php
+++ includes/Features/Title_Generation/Title_Generation.php
@@ -7,6 +7,7 @@
 
 namespace WordPress\AI\Features\Title_Generation;
 
+use WordPress\AI\AssetLoader;
 use WordPress\AI\API_Request;
 use WordPress\AI\Abilities\Title_Generation as Title_Generation_Ability;
 use WordPress\AI\Abstracts\Abstract_Feature;
@@ -40,6 +41,7 @@ class Title_Generation extends Abstract_Feature {
 	 */
 	public function register(): void {
 		add_action( 'wp_abilities_api_init', array( $this, 'register_abilities' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
 	}
 
 	/**
@@ -103,5 +105,38 @@ class Title_Generation extends Abstract_Feature {
 		}
 
 		return $response;
+	}
+
+	/**
+	 * Enqueues and localizes the admin script.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $hook_suffix The current admin page hook suffix.
+	 */
+	public function enqueue_assets( string $hook_suffix ): void {
+		// Load asset in new post and edit post screens only.
+		if ( 'post.php' !== $hook_suffix && 'post-new.php' !== $hook_suffix ) {
+			return;
+		}
+
+		$screen = get_current_screen();
+
+		// Load the assets only if the post type supports titles and is not an attachment.
+		if (
+			! post_type_supports( $screen->post_type, 'title' ) ||
+			in_array( $screen->post_type, [ 'attachment' ], true )
+		) {
+			return;
+		}
+
+		AssetLoader::enqueue_script( 'title_generation', 'features/title-generation' );
+		AssetLoader::localize_script(
+			'title_generation',
+			'title_generation_data',
+			array(
+				'enabled' => $this->is_enabled(),
+			)
+		);
 	}
 }
