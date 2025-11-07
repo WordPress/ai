273c189352f1bef5d241bdbdb1432f4ffc45df93
Darin Kotter <darin.kotter@gmail.com>
2025-11-07 09:29:37 -0700

    Update comment

--- src/features/title-generation/index.tsx
+++ src/features/title-generation/index.tsx
@@ -12,7 +12,7 @@ import { BlockControls } from '@wordpress/block-editor';
 import TitleToolbar from './components/TitleToolbar';
 import TitleToolbarWrapper from './components/TitleToolbarWrapper';
 
-// For template editing mode (when title is a block)
+// For template preview mode (when title is a block)
 // Use filter to add toolbar to post-title block
 const withTitleToolbar = createHigherOrderComponent(
 	( BlockEdit ) => {
