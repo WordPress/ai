e4b792a1dfd365e6bb57f78afbba8884bb6eaf5f
Darin Kotter <darin.kotter@gmail.com>
2025-11-17 15:44:23 -0700

    Add some examples of how assets can be added to the src directory

---
+++ src/experiments/example-experiment/index.tsx
@@ -0,0 +1,12 @@
+// Example only.
+
+import React from 'react';
+import { __ } from '@wordpress/i18n';
+
+import './index.scss';
+
+const ExampleExperiment = () => {
+	return <div className="test">{ __( 'Example Experiment', 'ai' ) }</div>;
+};
+
+export default ExampleExperiment;
