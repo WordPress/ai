fe92889e862fb97a9976e0ac4e56d9317bf75af9
Felix Arntz <flixos90@gmail.com>
2025-12-01 09:33:17 -0800

    Fux PHPCS error.

--- includes/Asset_Loader.php
+++ includes/Asset_Loader.php
@@ -109,9 +109,10 @@ class Asset_Loader {
 			return;
 		}
 		wp_style_add_data( 'ai_' . $handle, 'rtl', 'replace' );
-		if ( is_rtl() ) {
-			wp_style_add_data( 'ai_' . $handle, 'path', $rtl_style_path );
+		if ( ! is_rtl() ) {
+			return;
 		}
+		wp_style_add_data( 'ai_' . $handle, 'path', $rtl_style_path );
 	}
 
 	/**
