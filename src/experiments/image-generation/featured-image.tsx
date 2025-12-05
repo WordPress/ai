3ea858bbf9aa730e00c5c098afb8df12b1041f19
Darin Kotter <darin.kotter@gmail.com>
2025-12-05 09:46:17 -0700

    Add the initial filtering of the featured image component

---
+++ src/experiments/image-generation/featured-image.tsx
@@ -0,0 +1,46 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * WordPress dependencies
+ */
+import { createElement } from '@wordpress/element';
+import { addFilter } from '@wordpress/hooks';
+
+/**
+ * Internal dependencies
+ */
+import GenerateFeaturedImage from './components/generate-featured-image';
+
+const { aiImageGenerationData } = window as any;
+
+/**
+ * Wraps the PostFeaturedImage component to add a generate featured image button.
+ *
+ * @param {React.ComponentType<any>} OriginalComponent - The original PostFeaturedImage component.
+ * @return {React.ComponentType<any>} The wrapped component.
+ */
+function wrapPostFeaturedImage(
+	OriginalComponent: React.ComponentType< any >
+) {
+	if ( ! aiImageGenerationData.enabled ) {
+		return OriginalComponent;
+	}
+
+	return function ( props: any ) {
+		return createElement(
+			React.Fragment,
+			{},
+			<GenerateFeaturedImage />,
+			createElement( OriginalComponent, props )
+		);
+	};
+}
+
+addFilter(
+	'editor.PostFeaturedImage',
+	'ai/image-generation',
+	wrapPostFeaturedImage
+);
