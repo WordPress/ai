8223719099e3033666f4001ac6097ba91bd24b02
Darin Kotter <darin.kotter@gmail.com>
2026-02-21 08:09:50 -0700

    Add a class that will load the AI Client if needed

---
+++ includes/Client_Loader.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Client Loader file for the AI Experiments plugin.
+ *
+ * Handles loading the WP AI Client if needed.
+ *
+ * @package WordPress\AI
+ */
+
+declare( strict_types=1 );
+
+namespace WordPress\AI;
+
+use WordPress\AI_Client\AI_Client;
+
+/**
+ * Client Loader class.
+ *
+ * @since x.x.x
+ */
+final class Client_Loader {
+
+	/**
+	 * Whether the WP AI Client has been initialized.
+	 *
+	 * @since x.x.x
+	 * @var bool
+	 */
+	private bool $initialized = false;
+
+	/**
+	 * Checks if the WP AI Client is loaded.
+	 *
+	 * @since x.x.x
+	 *
+	 * @return bool True if the WP AI Client is loaded, false otherwise.
+	 */
+	public static function client_exists(): bool {
+		return function_exists( 'wp_ai_client_prompt' );
+	}
+
+	/**
+	 * Initializes the WP AI Client if needed.
+	 *
+	 * @since x.x.x
+	 */
+	public function init(): void {
+		if ( $this->initialized || self::client_exists() ) {
+			return;
+		}
+
+		AI_Client::init();
+
+		$this->initialized = true;
+	}
+}
