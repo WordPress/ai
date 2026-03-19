2aabeb63296fa1879e33d5ff60c7313c5b693a89
Nischay <itsnischay2604@gmail.com>
2026-03-19 23:17:50 +0530

    Fix lint and PHPStan issues in Refine_Notes enqueue_assets

--- includes/Experiments/Refine_Notes/Refine_Notes.php
+++ includes/Experiments/Refine_Notes/Refine_Notes.php
@@ -83,9 +83,9 @@ class Refine_Notes extends Abstract_Feature {
 	public function enqueue_assets(): void {
 		Asset_Loader::enqueue_script( 'refine_notes', 'experiments/refine-notes' );
 
-		$post_type        = get_post_type( get_the_ID() );
+		$post_type        = get_post_type();
 		$post_type_object = $post_type ? get_post_type_object( $post_type ) : null;
-		$rest_base        = ( $post_type_object && $post_type_object->rest_base )
+		$rest_base        = $post_type_object && $post_type_object->rest_base
 			? $post_type_object->rest_base
 			: null;
 
