285d58bc3c0a60a0b3efc4907bbb73ea34d308c4
Jeffrey Paul <jeffpaul@hotmail.com>
2026-04-09 11:26:02 -0500

    bump version to 0.7.0

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -29,7 +29,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Define plugin constants.
 if ( ! defined( 'WPAI_VERSION' ) ) {
-	define( 'WPAI_VERSION', '0.6.0' );
+	define( 'WPAI_VERSION', '0.7.0' );
 }
 if ( ! defined( 'WPAI_PLUGIN_FILE' ) ) {
 	define( 'WPAI_PLUGIN_FILE', defined( 'WPAI_DIR' ) ? WPAI_DIR . 'ai.php' : '' );
