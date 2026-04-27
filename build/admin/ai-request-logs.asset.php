282c1bc0e342adaecab708e2bbef5bbadabf6ca0
Tyler Bailey <tyler.bailey@10up.com>
2026-04-27 12:03:39 -0400

    Remove build assets from VCS. Add plugin icon SVG file.

--- build/admin/ai-request-logs.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n', 'wp-notices'), 'version' => '3e5d750e5e22370ddbd4');
