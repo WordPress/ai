262e55de739de42f6afcb3e235250be23f3b5966
Darin Kotter <darin.kotter@gmail.com>
2026-05-05 14:46:24 -0600

    Add some basic styles to the AI Summary Group variation block and load those in the editor and front-end

--- tests/Integration/Includes/Abilities/SummarizationTest.php
+++ tests/Integration/Includes/Abilities/SummarizationTest.php
@@ -499,7 +499,7 @@ class SummarizationTest extends WP_UnitTestCase {
 	/**
 	 * Test that execute_callback() preserves newlines in the output.
 	 *
-	 * @since 0.9.0
+	 * @since x.x.x
 	 */
 	public function test_execute_callback_preserves_newlines_in_output(): void {
 		$ability = new class(
