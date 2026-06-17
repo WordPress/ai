ef4fe4ce1cdda7bcd48d701e5451b37cabf3638e
Darin Kotter <darin.kotter@gmail.com>
2026-06-17 09:47:36 -0600

    Fix lint errors; remove unnecessary check

--- includes/Abilities/Content_Resizing/Content_Resizing.php
+++ includes/Abilities/Content_Resizing/Content_Resizing.php
@@ -170,13 +170,12 @@ class Content_Resizing extends Abstract_Ability {
 				);
 			}
 
-				// Ensure the post type is allowed in REST endpoints.
-				$post_type = get_post_type( $post_id );
-				$post_type_obj = get_post_type_object( $post_type );
+			// Ensure the post type is allowed in REST endpoints.
+			$post_type_obj = get_post_type_object( $post->post_type );
 
-				if ( empty( $post_type_obj->show_in_rest ) ) {
-					return false;
-				}
+			if ( ! $post_type_obj || empty( $post_type_obj->show_in_rest ) ) {
+				return false;
+			}
 		} elseif ( ! current_user_can( 'edit_posts' ) ) {
 			return new WP_Error(
 				'insufficient_capabilities',
