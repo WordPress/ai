add947430941ca000b16ca0d3564e9c79f069847
Roberto Iacono <roberiacono@gmail.com>
2026-07-04 01:58:09 +0200

    fix lint warnings

--- routes/ai-home/stage.tsx
+++ routes/ai-home/stage.tsx
@@ -232,7 +232,9 @@ function getPageData(): PageData {
 			hasCredentials: Boolean( pageData.hasCredentials ),
 			hasValidCredentials: Boolean( pageData.hasValidCredentials ),
 			connectorsUrl: toStringValue( pageData.connectorsUrl ),
-			connectorApprovalUrl: toStringValue( pageData.connectorApprovalUrl ),
+			connectorApprovalUrl: toStringValue(
+				pageData.connectorApprovalUrl
+			),
 			hasApprovedConnector: Boolean( pageData.hasApprovedConnector ),
 			featureGroups,
 			features,
